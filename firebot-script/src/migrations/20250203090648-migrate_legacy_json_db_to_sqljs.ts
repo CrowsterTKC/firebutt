bd32e74ba37ad6b58358bcd914db30c4a2217bb0
CrowsterTKC <crowstertkc@gmail.com>
2025-03-31 21:33:42 -0700

    Refactor script to fix errors with entity changes

--- firebot-script/src/migrations/20250203090648-migrate_legacy_json_db_to_sqljs.ts
+++ firebot-script/src/migrations/20250203090648-migrate_legacy_json_db_to_sqljs.ts
@@ -1,16 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-import {
-  Column,
-  CreateDateColumn,
-  DeleteDateColumn,
-  Entity,
-  MigrationInterface,
-  PrimaryColumn,
-  QueryRunner,
-  UpdateDateColumn,
-} from 'typeorm';
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 import { newGuid } from '../guid-handler';
 import { getFirebotProfileDataFolderPath } from '../utils/file-system';
@@ -23,73 +14,17 @@ type PhraseEntry = {
   createdByUser: string;
 };
 
-@Entity({ name: 'phrases' })
-class Phrase {
-  @PrimaryColumn()
-  id!: string;
-
-  @Column()
-  guid!: string;
-
-  @Column({ name: 'original_phrase', type: 'simple-json' })
-  originalPhrase!: string[];
-
-  @Column({ name: 'replacement_phrase' })
-  replacementPhrase!: string;
-
-  @Column({ name: 'part_of_speech', nullable: true, type: 'varchar' })
-  partOfSpeech: string | null = null;
-
-  @Column({ name: 'expires_at', nullable: true, type: 'datetime' })
-  expiresAt: Date | null = null;
-
-  @Column({ name: 'created_by_user' })
-  createdByUser!: string;
-
-  @CreateDateColumn({ name: 'inserted_at' })
-  insertedAt!: Date;
-
-  @UpdateDateColumn({ name: 'updated_at' })
-  updatedAt!: Date;
-
-  @DeleteDateColumn({ name: 'deleted_at', nullable: true })
-  deletedAt: Date | null = null;
-}
-
-@Entity({ name: 'usage_statistics' })
-class UsageStatistic {
-  @PrimaryColumn()
-  id!: string;
-
-  @Column()
-  guid!: string;
-
-  @Column({ name: 'original_phrase' })
-  originalPhrase!: string;
-
-  @Column({ name: 'replacement_phrase' })
-  replacementPhrase!: string;
-
-  @Column()
-  user!: string;
-
-  @Column({ name: 'original_message' })
-  originalMessage!: string;
-
-  @Column({ name: 'replacement_message' })
-  replacementMessage!: string;
-
-  @Column({ name: 'stream_title' })
-  streamTitle!: string;
-
-  @Column({ name: 'response_probability' })
-  responseProbability!: number;
-
-  @CreateDateColumn({ name: 'inserted_at' })
-  insertedAt!: Date;
-
-  @UpdateDateColumn({ name: 'updated_at' })
-  updatedAt!: Date;
+interface Phrase {
+  id: string;
+  guid: string;
+  original_phrase: string;
+  replacement_phrase: string;
+  part_of_speech: string | null;
+  expires_at: Date | null;
+  created_by_user: string;
+  inserted_at: Date;
+  updated_at: Date;
+  deleted_at: Date | null;
 }
 
 export class MigrateLegacyJsonDbToSqljs1738602408488
@@ -100,10 +35,6 @@ export class MigrateLegacyJsonDbToSqljs1738602408488
   }
 
   async up(queryRunner: QueryRunner): Promise<void> {
-    const phraseRepository = queryRunner.manager.getRepository('Phrase');
-    const usageStatisticRepository =
-      queryRunner.manager.getRepository('UsageStatistic');
-
     const filePath = path.join(
       getFirebotProfileDataFolderPath(),
       'firebutt.json'
@@ -118,31 +49,41 @@ export class MigrateLegacyJsonDbToSqljs1738602408488
     );
 
     for (const [originalPhrase, entry] of Object.entries(data)) {
-      const existingPhrase = await phraseRepository.findOne({
-        where: { replacementPhrase: entry.replacementPhrase },
-      });
+      const existingPhrases = (await queryRunner.query(
+        'SELECT * FROM phrases WHERE replacement_phrase = ? AND deleted_at IS NULL',
+        [entry.replacementPhrase]
+      )) as Phrase[];
 
-      if (existingPhrase) {
-        const updatedPhrase = phraseRepository.merge(existingPhrase, {
-          originalPhrase: [...existingPhrase.originalPhrase, originalPhrase],
-        });
-
-        await phraseRepository.save(updatedPhrase);
+      if (existingPhrases.length > 0) {
+        for (const existingPhrase of existingPhrases) {
+          await queryRunner.query(
+            'UPDATE phrases SET original_phrase = ? WHERE id = ?',
+            [
+              JSON.stringify([
+                ...JSON.parse(existingPhrase.original_phrase),
+                originalPhrase,
+              ]),
+              existingPhrase.id,
+            ]
+          );
+        }
       } else {
         const { id: phraseId, guid: phraseGuid } = newGuid({ type: 'phrase' });
-        const newPhrase = phraseRepository.merge(new Phrase(), {
-          id: phraseId,
-          guid: phraseGuid,
-          originalPhrase: [originalPhrase],
-          replacementPhrase: entry.replacementPhrase,
-          partOfSpeech: entry.partOfSpeech,
-          expiresAt: entry.expiresAt,
-          createdByUser: entry.createdByUser,
-          insertedAt: new Date(0),
-          updatedAt: new Date(),
-        });
-
-        await phraseRepository.save(newPhrase);
+        await queryRunner.query(
+          `INSERT INTO phrases (id, guid, original_phrase, replacement_phrase, part_of_speech, expires_at, created_by_user, inserted_at, updated_at)
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+          [
+            phraseId,
+            phraseGuid,
+            JSON.stringify([originalPhrase]),
+            entry.replacementPhrase,
+            entry.partOfSpeech,
+            entry.expiresAt,
+            entry.createdByUser,
+            new Date(0).toISOString(),
+            new Date().toISOString(),
+          ]
+        );
       }
 
       const nowISOString = new Date().toISOString();
@@ -153,38 +94,34 @@ export class MigrateLegacyJsonDbToSqljs1738602408488
           type: 'usage_statistic',
         });
 
-        const newUsageStatistic = usageStatisticRepository.merge(
-          new UsageStatistic(),
-          {
-            id: statId,
-            guid: statGuid,
-            originalPhrase: originalPhrase,
-            replacementPhrase: entry.replacementPhrase,
-            user: 'unknown',
-            originalMessage: 'unknown',
-            replacementMessage: 'unknown',
-            streamTitle: 'unknown',
-            responseProbability: 0.0,
-            insertedAt: nowISOString,
-            updatedAt: updatedAtISOString,
-          }
+        await queryRunner.query(
+          `INSERT INTO usage_statistics (id, guid, original_phrase, replacement_phrase, user, original_message, replacement_message, stream_title, response_probability, inserted_at, updated_at)
+          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+          [
+            statId,
+            statGuid,
+            originalPhrase,
+            entry.replacementPhrase,
+            'unknown',
+            'unknown',
+            'unknown',
+            'unknown',
+            0.0,
+            nowISOString,
+            updatedAtISOString,
+          ]
         );
-        await usageStatisticRepository.save(newUsageStatistic);
       }
     }
   }
 
   async down(queryRunner: QueryRunner): Promise<void> {
-    const phraseRepository = queryRunner.manager.getRepository('Phrase');
-    const usageStatisticRepository =
-      queryRunner.manager.getRepository('UsageStatistic');
-
-    await phraseRepository.delete({ insertedAt: new Date(0).toISOString() });
-    await usageStatisticRepository.delete({
-      user: 'unknown',
-      originalMessage: 'unknown',
-      replacementMessage: 'unknown',
-      streamTitle: 'unknown',
-    });
+    await queryRunner.query('DELETE FROM phrases WHERE inserted_at = ?', [
+      new Date(0).toISOString(),
+    ]);
+    await queryRunner.query(
+      'DELETE FROM usage_statistics WHERE user = ? AND original_message = ? AND replacement_message = ? AND stream_title = ?',
+      ['unknown', 'unknown', 'unknown', 'unknown']
+    );
   }
 }
